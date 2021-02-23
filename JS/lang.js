cf5cc7c52f3394e5eb22a5682726fa837ae08f38
MaloDaHood <malomr2002@gmail.com>
2021-02-23 22:19:57 +0100

    maquette presque finie

---
+++ JS/lang.js
@@ -0,0 +1,14 @@
+function langC() {
+    $('.langages').toggleClass('active')
+    $('.prog_c').toggleClass('active')
+}
+
+function langPython() {
+    $('.langages').toggleClass('active')
+    $('.prog_python').toggleClass('active')
+}
+
+function langJava() {
+    $('.langages').toggleClass('active')
+    $('.prog_java').toggleClass('active')
+}
